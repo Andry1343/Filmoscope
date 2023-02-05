1d33e51cecf509a12c3d977598fc90c04eb5b606
Andry1343 <ua.gavrilets@gmail.com>
2023-02-05 22:23:01 +0200

    Start

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './Layout/layout.jsx';
+import { NotFound } from './pages/NotFound/NotFound';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Movies = lazy(() => import('./pages/Movies/Movies'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const MovieDetails = lazy(() => import('./pages/MovieDetails/MovieDetails'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
